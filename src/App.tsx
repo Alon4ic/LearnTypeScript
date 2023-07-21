4b3f18fb98e0e4846dbe207dc5a31ccd8e817421
Alon4ic <o.redjko@gmail.com>
2023-07-21 13:13:08 +0300

    Creating classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React, { Suspense } from 'react';
-import Counter from './components/Counter';
-import './index.scss';
-import { Route, Routes, Link } from 'react-router-dom';
-import AboutPage from './pages/AboutPage/AboutPage';
-import MainPage from './pages/MainPage/MainPage';
+import React, { Suspense, useContext, useState } from 'react';
+import './styles/index.scss';
+import { Link, Route, Routes } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import {Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
+
+
+
 
 const App = () => {
+	const {theme, toggleTheme} = useTheme();
+	const boolean = true;
 	return (
-		<div className="app">
+		<div className={classNames('app', {}, [theme])}>
+			<button onClick={toggleTheme}>TOGGLE</button>
 			<Link to={'/'}>Home</Link>
 			<Link to={'/about'}>About us</Link>
 			<Suspense fallback = {<div>loading...</div>}>
